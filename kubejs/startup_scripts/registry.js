b358ff06c40c828e154ba7c9ae75457c22deadc5
DancingSnow <1121149616@qq.com>
2024-05-13 01:24:45 +0800

    谷神星

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -5,13 +5,25 @@ StartupEvents.registry('item', event => {
 })
 
 StartupEvents.registry('block', event => {
+    // 火卫二
     event.create('deimos_stone')
         .stoneSoundType()
         .hardness(2)
         .requiresTool()
         .tagBlock("mineable/pickaxe")
-
     event.create('deimos_surface')
+        .stoneSoundType()
+        .hardness(2)
+        .requiresTool()
+        .tagBlock("mineable/pickaxe")
+
+    // 谷神星
+    event.create('ceres_stone')
+        .stoneSoundType()
+        .hardness(2)
+        .requiresTool()
+        .tagBlock("mineable/pickaxe")
+    event.create('ceres_surface')
         .stoneSoundType()
         .hardness(2)
         .requiresTool()
