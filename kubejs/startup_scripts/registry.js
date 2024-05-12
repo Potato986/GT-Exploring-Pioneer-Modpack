7b439aa00e63f642867dab97768af81c85b71920
DancingSnow <1121149616@qq.com>
2024-05-12 21:55:53 +0800

    添加火卫二

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -2,5 +2,18 @@ StartupEvents.registry('item', event => {
     event.create('twilight_crystal')
         .tooltip(Text.translatable('tooltip.kubejs.twilight_crystal'))
         .rarity('uncommon')
-        
+})
+
+StartupEvents.registry('block', event => {
+    event.create('deimos_stone')
+        .stoneSoundType()
+        .hardness(2)
+        .requiresTool()
+        .tagBlock("mineable/pickaxe")
+
+    event.create('deimos_surface')
+        .stoneSoundType()
+        .hardness(2)
+        .requiresTool()
+        .tagBlock("mineable/pickaxe")
 })
